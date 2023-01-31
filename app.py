4709fec71483106ee764e0240285978abeaad498
weirdcease <78571139+weirdcease@users.noreply.github.com>
2023-01-31 14:08:49 -0500

    start making the app

--- app.py
+++ app.py
@@ -9,32 +9,59 @@ BR = "\u2518"
 
 def main(stdscr):
     curses.curs_set(0)
+    active_note = None
+    mode = "menu"
+    active_item = 0
+    notes = ["one", "two", "three"]
 
-    menu_width = int((1 / 3) * curses.COLS)
-    editor_width = curses.COLS - menu_width
-    stdscr.addstr(TL)
-    stdscr.addstr(HL * (menu_width - 2))
-    stdscr.addstr(TR)
-    stdscr.addstr(TL)
-    stdscr.addstr(HL * (editor_width - 2))
-    stdscr.addstr(TR)
-    
-    for line in range(curses.LINES - 2):
-        y = line + 1
-        stdscr.addstr(y, 0, VL)
-        stdscr.addstr(y, menu_width, VL)
-        stdscr.addstr(y, menu_width - 1, VL)
-        stdscr.addstr(y, menu_width + editor_width - 1, VL)
+    while True:
+        stdscr.clear()
+        rows, cols = stdscr.getmaxyx()
 
-    stdscr.addstr(BL)
-    stdscr.addstr(HL * (menu_width - 2))
-    stdscr.addstr(BR)
-    stdscr.addstr(BL)
-    stdscr.addstr(HL * (editor_width - 2))
-    #stdscr.addstr(BR)
+        if active_note:
+            active_title = "blah blah blah"
+        else:
+            active_title = "No note selected"
 
-    stdscr.getkey()
+        menu_width = int((1 / 3) * cols)
+        editor_width = cols - menu_width
+        stdscr.addstr(0, 0, TL + HL + " CeaseNotes " + HL * (menu_width - 15) + TR)
+        stdscr.addstr(0, menu_width, TL + HL + " " + active_title + " " + HL * (editor_width - (len(active_title) + 5)) + TR)
+        
+        for line in range(rows - 2):
+            y = line + 1
+            stdscr.addstr(y, 0, VL)
+            stdscr.addstr(y, menu_width, VL)
+            stdscr.addstr(y, menu_width - 1, VL)
+            stdscr.addstr(y, menu_width + editor_width - 1, VL)
+
+        stdscr.addstr(BL)
+        stdscr.addstr(HL * (menu_width - 2))
+        stdscr.addstr(BR)
+        stdscr.addstr(BL)
+        stdscr.addstr(HL * (editor_width - 2))
+        try:
+            stdscr.addstr(BR)
+        except:
+            pass
+
+        key = stdscr.getkey()
+
+        stdscr.addstr(0, 0, key)
+
+        key = stdscr.getkey()
+
+        if key in ["^[", "KEY_ESCAPE"]:
+            if mode == "menu":
+                mode = "askclose"
+
+            else:
+                mode = "menu"
+
+        elif mode == "menu":
+            if key == "KEY_DOWN":
+                active_item += 1
 
 if __name__ == "__main__":
     message = curses.wrapper(main)
-    print(message)
+    if message: print(message)
