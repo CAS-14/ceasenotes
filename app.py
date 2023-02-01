497eb79fc40eebc16e0544902a6fcf2a3f5d1b04
weirdcease <78571139+weirdcease@users.noreply.github.com>
2023-02-01 14:29:17 -0500

    aaa

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import curses
+import os
 
 HL = "\u2500"
 VL = "\u2502"
@@ -7,21 +8,24 @@ TR = "\u2510"
 BL = "\u2514"
 BR = "\u2518"
 
+log = open("log", "w")
+
 def main(stdscr):
     curses.curs_set(0)
-    active_note = None
+    
     mode = "menu"
-    active_item = 0
-    notes = ["one", "two", "three"]
+    active = 0
+    
+    titles = ["one", "two", "three"]
+    bodies = ["oneneone", "this my note", "threeeed"]
 
     while True:
         stdscr.clear()
         rows, cols = stdscr.getmaxyx()
 
-        if active_note:
-            active_title = "blah blah blah"
-        else:
-            active_title = "No note selected"
+        active_title = titles[active]
+
+        log.write(mode)
 
         menu_width = int((1 / 3) * cols)
         editor_width = cols - menu_width
@@ -35,33 +39,61 @@ def main(stdscr):
             stdscr.addstr(y, menu_width - 1, VL)
             stdscr.addstr(y, menu_width + editor_width - 1, VL)
 
-        stdscr.addstr(BL)
-        stdscr.addstr(HL * (menu_width - 2))
-        stdscr.addstr(BR)
-        stdscr.addstr(BL)
-        stdscr.addstr(HL * (editor_width - 2))
+        stdscr.addstr(BL + HL * (menu_width - 2) + BR + BL + HL * (editor_width - 2))
         try:
             stdscr.addstr(BR)
         except:
             pass
 
+        if mode == "askclose":
+            ask_y = int((rows - 2) / 2)
+            x_center = int((menu_width - 2) / 2)
+            stdscr.addstr(ask_y, x_center - 5, "Press ENTER")
+            stdscr.addstr(ask_y + 1, x_center - 3, "to exit")
+
+        else:
+            for index in range(len(titles)):
+                if active == index:
+                    if mode == "menu":
+                        style = curses.A_UNDERLINE
+                    else:
+                        style = curses.A_STANDOUT
+                else:
+                    style = curses.A_NORMAL
+
+                stdscr.addstr(index + 1, 1, titles[index], style)
+
         key = stdscr.getkey()
 
-        stdscr.addstr(0, 0, key)
-
-        key = stdscr.getkey()
-
-        if key in ["^[", "KEY_ESCAPE"]:
-            if mode == "menu":
+        if mode == "menu":
+            if key == "^[":
                 mode = "askclose"
+            elif key == "KEY_DOWN":
+                active += 1
+                if active > len(titles) - 1:
+                    active = 0
+            elif key == "KEY_UP":
+                active -= 1
+                if active < 0:
+                    active = len(titles) - 1
+            elif key == "\n":
+                mode = "note"
 
+        elif mode == "note":
+            if key == "^[":
+                mode = "menu"
+            else:
+                pass
+
+        elif mode == "askclose":
+            if key == "\n":
+                break
             else:
                 mode = "menu"
 
-        elif mode == "menu":
-            if key == "KEY_DOWN":
-                active_item += 1
-
 if __name__ == "__main__":
+    os.environ.setdefault("ESCDELAY", "25")
     message = curses.wrapper(main)
     if message: print(message)
+
+    log.close()
